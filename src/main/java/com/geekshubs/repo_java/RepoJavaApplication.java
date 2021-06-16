58fcb9f82b410412bdfaba023a57bd3d864c7888
Xavi <xavi@geekshubs.com>
2021-06-16 14:08:59 +0200

    first commit

---
+++ src/main/java/com/geekshubs/repo_java/RepoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.geekshubs.repo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geekshubs/repo_java/RepoJavaApplicationTests.java
@@ -0,0 +1,21 @@
+package com.geekshubs.repo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class RepoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void firstTest(){
+
+        assertTrue(true);
+    }
+
+}
